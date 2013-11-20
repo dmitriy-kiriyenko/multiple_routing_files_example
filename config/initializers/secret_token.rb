7a2de97cefdb65d2e466a4f7a7e99c7217d4ea7e
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2013-11-20 14:33:06 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultipleRoutingFilesExample::Application.config.secret_key_base = '0269e42c33db34c51432d2413252c916cb5e7d5a55634d571e1f371b290e576b6726b043a893747c77424d31f8815a57cb4ec45fdb09e2f2336fcbe39045933c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MultipleRoutingFilesExample::Application.config.session_store :cookie_store, key: '_multiple_routing_files_example_session'
